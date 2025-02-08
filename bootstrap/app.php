a1d41eba32a461bea75ad40c312ec2380c6f1ca2
jhon landeo <jhon.landeo@alarcongroup.us>
2025-02-08 10:53:32 -0500

    fix:

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,12 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
+    // ->withMiddleware(function (Middleware $middleware) {
+    //     $middleware->api(prepend: [
+    //         \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+    //     ]);
         
-    })
+    // })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
