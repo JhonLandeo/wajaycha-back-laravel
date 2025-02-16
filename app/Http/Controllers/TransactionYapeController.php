3e0ae5381b9e4c5fe75d62d1e4043b3cbf8dd595
jhon landeo <jhon.landeo@alarcongroup.us>
2025-02-16 17:48:12 -0500

    feat:

--- app/Http/Controllers/TransactionYapeController.php
+++ app/Http/Controllers/TransactionYapeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Imports\TransactionYapeImport;
 use App\Models\TransactionYape;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Facades\Excel;
 
 class TransactionYapeController extends Controller
@@ -12,6 +13,7 @@ class TransactionYapeController extends Controller
     public function import(Request $request)
     {
         try {
+            Log::info($request->user_id);
             $file = $request->file('file');
             Excel::import(new TransactionYapeImport(), $file);
             return response()->json(['status' => 'ok']);
--- app/Imports/TransactionYapeImport.php
+++ app/Imports/TransactionYapeImport.php
@@ -19,41 +19,41 @@ class TransactionYapeImport implements ToModel, WithHeadingRow
     //  *
     //  * @return int
     //  */
-    // public function headingRow(): int
-    // {
-    //     return 5; // Empezar a leer encabezados desde la fila 5
-    // }
+    public function headingRow(): int
+    {
+        return 5; // Empezar a leer encabezados desde la fila 5
+    }
 
     // /**
     //  * @param array $row
     //  *
     //  * @return \Illuminate\Database\Eloquent\Model|null
     //  */
-    // public function model(array $row)
-    // {
-    // //    Log::info($row);
-    //     $dateOperation = null;
-    //     LOg::info($row['Fecha de operación']);
+    public function model(array $row)
+    {
+        $dateOperation = null;
 
-    //     if (!empty($row['Fecha de operación'])) {
-    //         $dateString = $row['Fecha de operación'];
+        if (!empty($row['Fecha de operación'])) {
+            $dateString = $row['Fecha de operación'];
+            Log::info($dateString);
 
-    //         // Intentar diferentes formatos de fecha
-    //         if (Carbon::hasFormat($dateString, 'd/m/Y H:i:s')) {
-    //             $dateOperation = Carbon::createFromFormat('d/m/Y H:i:s', $dateString)->format('Y-m-d H:i:s');
-    //         } elseif (Carbon::hasFormat($dateString, 'd/m/Y')) {
-    //             $dateOperation = Carbon::createFromFormat('d/m/Y', $dateString)->format('Y-m-d') . ' 00:00:00';
-    //         }
-    //     }
-    //     return new TransactionYape([
-    //         'message' => $row['Mensaje'],
-    //         'origin' => $row['Origen'],
-    //         'destination' => $row['Destino'],
-    //         'amount' => (float) $row['Monto'],
-    //         'date_operation' => $dateOperation,
-    //         'type_transaction' => $row['Tipo de Transacción'] == 'PAGASTE' ? 'expense' : 'income',
-    //     ]);
-    // }
+            // Intentar diferentes formatos de fecha
+            if (Carbon::hasFormat($dateString, 'd/m/Y H:i:s')) {
+                $dateOperation = Carbon::createFromFormat('d/m/Y H:i:s', $dateString)->format('Y-m-d H:i:s');
+            } elseif (Carbon::hasFormat($dateString, 'd/m/Y')) {
+                $dateOperation = Carbon::createFromFormat('d/m/Y', $dateString)->format('Y-m-d') . ' 00:00:00';
+            }
+        }
+        return new TransactionYape([
+            'message' => $row['Mensaje'],
+            'origin' => $row['Origen'],
+            'destination' => $row['Destino'],
+            'amount' => (float) $row['Monto'],
+            'date_operation' => $dateOperation,
+            'type_transaction' => $row['Tipo de Transacción'] == 'PAGASTE' ? 'expense' : 'income',
+            'user_id' => 1,
+        ]);
+    }
 
 
 
@@ -64,33 +64,33 @@ class TransactionYapeImport implements ToModel, WithHeadingRow
      *
      * @return int
      */
-    public function headingRow(): int
-    {
-        return 3; // Empezar a leer encabezados desde la fila 5
-    }
+    // public function headingRow(): int
+    // {
+    //     return 3; // Empezar a leer encabezados desde la fila 5
+    // }
 
     /**
      * @param array $row
      *
      * @return \Illuminate\Database\Eloquent\Model|null
      */
-    public function model(array $row)
-    {
-        // Convertir Fecha Operación
-        $fechaOperacion = Date::excelToDateTimeObject($row['Fecha Operación'])->format('Y-m-d');
+    // public function model(array $row)
+    // {
+    //     // Convertir Fecha Operación
+    //     $fechaOperacion = Date::excelToDateTimeObject($row['Fecha Operación'])->format('Y-m-d');
     
-        // Convertir Hora Operación
-        $horaOperacion = gmdate('H:i:s', floor($row['Hora Operación'] * 86400)); // Fracción del día a HH:MM:SS
+    //     // Convertir Hora Operación
+    //     $horaOperacion = gmdate('H:i:s', floor($row['Hora Operación'] * 86400)); // Fracción del día a HH:MM:SS
     
-        Log::info($row);
+    //     Log::info($row);
     
-        return new TransactionYape([
-            'message' => null,
-            'origin' => $row['NOMBRE Origen'],
-            'destination' => $row['NOMBRE Destino'],
-            'amount' => (float) $row['Monto'],
-            'date_operation' => $fechaOperacion . ' ' . $horaOperacion,
-            'type_transaction' => $row['Tipo de Operación'] == 'YAPEASTE' ? 'expense' : 'income',
-        ]);
-    }
+    //     return new TransactionYape([
+    //         'message' => null,
+    //         'origin' => $row['NOMBRE Origen'],
+    //         'destination' => $row['NOMBRE Destino'],
+    //         'amount' => (float) $row['Monto'],
+    //         'date_operation' => $fechaOperacion . ' ' . $horaOperacion,
+    //         'type_transaction' => $row['Tipo de Operación'] == 'YAPEASTE' ? 'expense' : 'income',
+    //     ]);
+    // }
 }
