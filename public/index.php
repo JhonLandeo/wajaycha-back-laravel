ce536f2a4a4772869717aecd0dc73e85f2d282e2
jhon landeo <jhon.landeo@alarcongroup.us>
2025-10-13 20:26:04 -0500

    change name index

--- public/index.php
+++ public/index.php
@@ -5,13 +5,13 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../../expenses-back/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../../wajaycha-back-laravel/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../../expenses-back/vendor/autoload.php';
+require __DIR__.'/../../wajaycha-back-laravel/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../../expenses-back/bootstrap/app.php')
+(require_once __DIR__.'/../../wajaycha-back-laravel/bootstrap/app.php')
     ->handleRequest(Request::capture());
