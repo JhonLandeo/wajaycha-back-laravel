39d55a97f80e5fbb491a4c28bcb36568502515e9
jhon landeo <jhon.landeo@alarcongroup.us>
2025-10-14 22:46:31 -0500

    se cambio el dominio del front

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://wajaycha.netlify.app')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://wajaycha.com')],
 
     'allowed_origins_patterns' => [],
 
