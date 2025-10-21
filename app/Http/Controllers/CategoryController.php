8e12a59eb2b40c0f82fa2e596502836bfe3fe323
jhon landeo <jhon.landeo@alarcongroup.us>
2025-10-20 21:44:11 -0500

    Refactor CategoryController: change order of categories to ascending by ID for consistent listing

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -23,7 +23,7 @@ class CategoryController extends Controller
         $categories = Category::query()
         ->where('user_id', $userId)
         ->with('paretoClassification')
-        ->orderBy('created_at', 'desc')
+        ->orderBy('id', 'asc')
         ->paginate($per_page, ['*'], 'page', $page);
 
         return response()->json($categories);
