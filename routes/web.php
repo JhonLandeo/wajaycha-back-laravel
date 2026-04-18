498b6abd7cab1c442d7f59f5bfb887c1c9a30771
jhon landeo <jhon.landeo@alarcongroup.us>
2026-04-18 17:16:04 -0500

    feat: add privacy policy page and configure agent development rules and workflows

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+
+Route::get('/privacy-policy', function () {
+    return view('privacy-policy');
+});
+
